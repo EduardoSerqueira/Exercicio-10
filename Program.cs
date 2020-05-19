9e99af6c7b51cbaad6e15ec14e90478989366e8b
e.serqueira <eduardo.serqueira@outlook.com>
2020-05-18 22:21:14 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercicio_dez
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+
+            Console.WriteLine("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero < 0)
+                Console.WriteLine("NEGATIVO");
+            else
+                Console.WriteLine("NÃO NEGATIVO");
+        }
+    }
+}
